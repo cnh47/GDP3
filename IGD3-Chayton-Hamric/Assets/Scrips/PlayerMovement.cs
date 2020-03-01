7fd8d99db2d0431be6147d464a0e1dc063e9b60b
Chayton Hamric <45079805+ChaytonH@users.noreply.github.com>
2020-03-01 16:09:37 -0500

    Start

---
+++ IGD3-Chayton-Hamric/Assets/Scrips/PlayerMovement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float t;
+    float t2;
+    public Vector3 startPosition;
+    public Vector3 target;
+    public Vector3 target2;
+    public float timeToReachTarget;
+    void Start()
+    {
+       this.transform.position = new Vector3(11.03f, 9.71f, 7.01f);
+
+    }
+    void Update()
+    {
+        
+
+        if (Input.GetButton("C"))
+        {
+            MovePlayerSS();
+
+        }
+
+        if (Input.GetButton("R"))
+        {
+            RunDive();
+            
+        }
+
+    }
+
+
+    public void RunDive()
+    {
+        t2 += Time.deltaTime / timeToReachTarget;
+        transform.position = Vector3.Lerp(target, target2, t2);
+        if (transform.position == target2)
+        {
+            GetComponent<Rigidbody>().useGravity = true;
+
+        }
+        else
+        {
+            GetComponent<Rigidbody>().useGravity = false;
+        }
+    }
+
+    public void MovePlayerSS()
+    {
+        
+        t += Time.deltaTime / timeToReachTarget;
+        transform.position = Vector3.Lerp(startPosition, target, t);
+        if (transform.position == target)
+        {
+            GetComponent<Rigidbody>().useGravity = true;
+
+        }
+        else
+        {
+            GetComponent<Rigidbody>().useGravity = false;
+        }
+    }
+
+
+    /*
+    public void SetDestination(Vector3 destination, float time)
+    {
+        t = 0;
+        startPosition = transform.position;
+        timeToReachTarget = time;
+        target = destination;
+    }
+    */
+}
---
+++ IGD3-Chayton-Hamric/Assets/Scrips/score.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour
+{
+    public Transform player;
+    public Text scoreText;
+    int num;
+    // Update is called once per frame
+    void Start()
+    {
+        num = 0;
+        scoreText.text = num.ToString();
+    }
+    void Update()
+    {
+        if(player.position.y < -1.28f & num == 0)
+        {
+            num = Random.Range(1, 11);
+            scoreText.text = num.ToString();
+        }
+    }
+}
